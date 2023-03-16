d1d2a738670031bfb5304ce5693183acf5b548ac
Lucas Santiago Santana <lucin189@gmail.com>
2023-03-16 09:34:13 -0300

    Add planets data loading before server finished initializng

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,7 @@ import http from "http";
 import * as dotenv from "dotenv";
 
 import { app } from "./app.js";
+import { loadPlanetsData } from "./models/planets-model.js";
 
 dotenv.config({});
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 8080;
 const protocol = process.env.NODE_ENV === "development" ? http : https;
 const server = protocol.createServer(app);
 
+await loadPlanetsData();
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
