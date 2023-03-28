764c243bef12c4ed7681c53652e880b169a8ac51
Lucas Santiago Santana <lucin189@gmail.com>
2023-03-27 22:26:19 -0300

    Implement httpSubmitLaunch function

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -12,8 +12,19 @@ async function httpGetLaunches() {
 }
 
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  try {
+    return await fetch(API_URL + "/launches", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "post",
+      body: JSON.stringify(launch),
+    });
+  } catch (e) {
+    return {
+      ok: false,
+    };
+  }
 }
 
 async function httpAbortLaunch(id) {
