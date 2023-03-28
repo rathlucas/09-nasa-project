d64be98771cf6a7a1836a002c3ad3bb72b158397
Lucas Santiago Santana <lucin189@gmail.com>
2023-03-27 22:27:38 -0300

    Update destination property for launches to target

--- server/src/models/launches-model.js
+++ server/src/models/launches-model.js
@@ -7,7 +7,7 @@ const launch = {
   mission: "Kepler Exploration X",
   rocket: "Explorer IS1",
   launchDate: new Date("december 27, 2030"),
-  destination: "Kepler-442b",
+  target: "Kepler-442b",
   customers: ["ZTM", "NASA"],
   upcoming: true,
   success: true,
