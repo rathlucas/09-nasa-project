c6ec2a10861c4610329891bee4ec5c3f03ae7718
Lucas Santiago Santana <lucin189@gmail.com>
2023-03-27 22:10:47 -0300

    Implement httpAddLaunch method

--- server/src/controllers/launches-controller.js
+++ server/src/controllers/launches-controller.js
@@ -1,7 +1,30 @@
-import { getLaunches } from "../models/launches-model.js";
+import { getLaunches, addLaunch } from "../models/launches-model.js";
 
 export class LaunchesController {
   static async httpGetLaunches(req, res) {
     return res.status(200).json(getLaunches());
+  }
+
+  static async httpAddLaunch(req, res) {
+    const launch = req.body;
+
+    if (
+      !launch.mission ||
+      !launch.rocket ||
+      !launch.launchDate ||
+      !launch.destination
+    ) {
+      return res
+        .status(400)
+        .json({ error: "Missing required launch property" });
+    }
+
+    launch.launchDate = new Date(launch.launchDate);
+    if (isNaN(launch.launchDate)) {
+      return res.status(400).json({ error: "Invalid launch date" });
+    }
+
+    addLaunch(launch);
+    return res.status(201).json(launch);
   }
 }
