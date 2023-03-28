eb795046087e011055c660274864b8bde64589e8
Lucas Santiago Santana <lucin189@gmail.com>
2023-03-27 21:16:18 -0300

    Add data access function to launches controller

--- server/src/controllers/launches-controller.js
+++ server/src/controllers/launches-controller.js
@@ -1,7 +1,7 @@
-import { launches } from "../models/launches-model.js";
+import { getLaunches } from "../models/launches-model.js";
 
 export class LaunchesController {
-  static async getLaunches(req, res) {
-    return res.status(200).json(Array.from(launches.values()));
+  static async httpGetLaunches(req, res) {
+    return res.status(200).json(getLaunches());
   }
 }
