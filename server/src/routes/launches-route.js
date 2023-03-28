2166cef535e5a62764cf5f315bcaac39be15a5d9
Lucas Santiago Santana <lucin189@gmail.com>
2023-03-27 22:11:12 -0300

    Create route handler for POST /launches

--- server/src/routes/launches-route.js
+++ server/src/routes/launches-route.js
@@ -4,5 +4,6 @@ import { LaunchesController } from "../controllers/launches-controller.js";
 const launchesRouter = express.Router();
 
 launchesRouter.get("/", LaunchesController.httpGetLaunches);
+launchesRouter.post("/", LaunchesController.httpAddLaunch);
 
 export { launchesRouter };
