e075959bab99d5179130d25346d21b2d22fd3e89
Lucas Santiago Santana <lucin189@gmail.com>
2023-03-28 22:58:49 -0300

    Create __tests__ dir

---
+++ server/__tests__/launches/launches.spec.js
@@ -0,0 +1,14 @@
+describe("Test GET /launches", () => {
+  it("Should respond with 200 success", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe("Test POST /launches", () => {
+  it("Should respond with 200 success", () => {});
+
+  it("Should catch missing required properties", () => {});
+
+  it("Should catch invalid dates", () => {});
+});
