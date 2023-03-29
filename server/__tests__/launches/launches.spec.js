fb13ef65f12c26a2b8b0d314ace265ee6944f01d
Lucas Santiago Santana <lucin189@gmail.com>
2023-03-28 23:29:57 -0300

    Ensure POST /launches responds with 201 created on success

--- server/__tests__/launches/launches.spec.js
+++ server/__tests__/launches/launches.spec.js
@@ -8,7 +8,31 @@ describe("Test GET /launches", () => {
 });
 
 describe("Test POST /launches", () => {
-  it("Should respond with 200 success", () => {});
+  const completeLaunchData = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701",
+    target: "kepler-186 f",
+    launchDate: "January 4, 2028",
+  };
+
+  const launchDataWithoutDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701",
+    target: "kepler-186 f",
+  };
+
+  it("Should respond with 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaunchData)
+      .expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    expect(responseDate).toBe(requestDate);
+
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+  });
 
   it("Should catch missing required properties", () => {});
 
