6779d1540a42f8a6fa8ea481cb99d3eae26b06ab
Lucas Santiago Santana <lucin189@gmail.com>
2023-04-04 22:51:46 -0300

    Move launches-model and crate launches-mongo

---
+++ server/src/models/launches/launches-mongo.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const launchesSchema = new mongoose.Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+    default: 100,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  upcoming: {
+    type: Boolean,
+    required: true,
+  },
+  success: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  customers: [String],
+});
