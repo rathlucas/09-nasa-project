ab12f7da542eb6c24b7b7ff50ad340a729159190
Lucas Santiago Santana <lucin189@gmail.com>
2023-04-05 09:12:33 -0300

    Make httpGetPlanets async

--- server/src/controllers/planets-controller.js
+++ server/src/controllers/planets-controller.js
@@ -1,7 +1,7 @@
 import { getPlanets } from "../models/planets/planets-model.js";
 
 export class PlanetsController {
-  static httpGetPlanets(req, res) {
-    return res.status(200).json(getPlanets());
+  static async httpGetPlanets(req, res) {
+    return res.status(200).json(await getPlanets());
   }
 }
