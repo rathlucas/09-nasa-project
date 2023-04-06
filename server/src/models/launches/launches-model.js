c5080dbd5f872069f9c58ece423eda6ab6b9df1e
Lucas Santiago Santana <lucin189@gmail.com>
2023-04-06 09:06:14 -0300

    Implement getLaunches and saveLaunch data access functions

--- server/src/models/launches/launches-model.js
+++ server/src/models/launches/launches-model.js
@@ -1,6 +1,5 @@
-// import launches from "./launches-mongo.js";
-
-const launches = new Map();
+import launches from "./launches-mongo.js";
+import planets from "../planets/planets-mongo.js";
 
 let latestFlightNumber = 100;
 
@@ -9,20 +8,40 @@ const launch = {
   mission: "Kepler Exploration X",
   rocket: "Explorer IS1",
   launchDate: new Date("december 27, 2030"),
-  target: "Kepler-442b",
+  target: "Kepler-442 b",
   customers: ["ZTM", "NASA"],
   upcoming: true,
   success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch);
 
 function existsLaunchWithId(launchId) {
   return launches.has(launchId);
 }
 
-function getLaunches() {
-  return Array.from(launches.values());
+async function getLaunches() {
+  return await launches.find({}, { _id: 0, __v: 0 });
+}
+
+async function saveLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error("No matching planet was found!");
+  }
+
+  await launches.updateOne(
+    {
+      flightNumber: launch.flightNumber,
+    },
+    launch,
+    {
+      upsert: true,
+    }
+  );
 }
 
 function addLaunch(launch) {
