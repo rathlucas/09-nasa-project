dff14670c67e3f4a14c826da930571540c9bd736
Lucas Santiago Santana <lucin189@gmail.com>
2023-04-10 08:49:29 -0300

    Move mongo connection code to own service

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,15 @@
 import https from "https";
 import http from "http";
-import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 
 import { app } from "./app.js";
+import { mongoConnect } from "./services/mongo.js";
 import { loadPlanetsData } from "./models/planets/planets-model.js";
 import { UndefinedException } from "./errors/undefined-exception.js";
 
 dotenv.config({});
 
 const PORT = process.env.PORT ?? 8080;
-
-const MONGO_URL = `mongodb+srv://lucas:${process.env.MONGO_PASSWORD}@cluster0.chbipbt.mongodb.net/nasa?retryWrites=true&w=majority`;
 
 if (typeof process.env["MONGO_PASSWORD"] == "undefined") {
   throw new UndefinedException(
@@ -28,18 +26,10 @@ if (typeof process.env["NODE_ENV"] == "undefined") {
 const protocol = process.env.NODE_ENV === "development" ? http : https;
 const server = protocol.createServer(app);
 
-mongoose.connection.on("open", () => {
-  console.log("MongoDB connection ready");
-});
-
-mongoose.connection.on("error", (e) => {
-  console.error(e);
-});
-
 (async () => {
-  await mongoose.connect(MONGO_URL);
-
+  await mongoConnect();
   await loadPlanetsData();
+
   server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
   });
