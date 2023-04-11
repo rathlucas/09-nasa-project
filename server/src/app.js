39c085efe1a65c2254c227bc9878eab61b764309
Lucas Santiago Santana <lucin189@gmail.com>
2023-04-11 08:32:00 -0300

    Mount v1 api router to app

--- server/src/app.js
+++ server/src/app.js
@@ -3,9 +3,8 @@ import express, { json } from "express";
 import cors from "cors";
 import morgan from "morgan";
 
-import { planetsRouter } from "./routes/planets-route.js";
-import { launchesRouter } from "./routes/launches-route.js";
 import { getDirname } from "./helpers/utils.js";
+import { api } from "./routes/api.js";
 
 export const app = express();
 
@@ -19,8 +18,8 @@ app.use(morgan("combined"));
 app.use(json());
 app.use(express.static(path.join(getDirname(import.meta.url), "..", "public")));
 
-app.use("/planets", planetsRouter);
-app.use("/launches", launchesRouter);
+app.use("/v1", api);
+
 app.get("/*", (req, res) => {
   res.sendFile(
     path.join(getDirname(import.meta.url), "..", "public", "index.html")
